e7bb8ab392e9b769584ae8f2244c8f8033dc6825
Francesco Pennacchio <francesco.pennacchio@studenti.unipd.it>
2024-05-10 20:14:36 +0200

    Aggiunti i CMakeFiles

---
+++ build/gdb/CMakeFiles/gdb3.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/fra01/lab_informatica/assegnamenti/assegnamento_gdb_valgrind_cmake-francescopennacchio/gdb/gdb3.c" "gdb/CMakeFiles/gdb3.dir/gdb3.c.o" "gcc" "gdb/CMakeFiles/gdb3.dir/gdb3.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
